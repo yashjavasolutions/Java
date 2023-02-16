350eba4174c530a7ec9caa6fb241bd7dcd5d9d11
Yash <yash.javasolutions@gmail.com>
2023-02-15 20:39:41 -0600

    Streams

--- JanBatch/src/com/yash/demo/HelloWorld.java
+++ JanBatch/src/com/yash/demo/HelloWorld.java
@@ -1,12 +1,15 @@
 package com.yash.demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
-
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 
 
@@ -19,7 +22,7 @@ public class HelloWorld {
 	Employee e3 = new Employee(3, "Manasa"); 
 	Employee e4 = new Employee(4, "Sharad"); 
 	Employee e5 = new Employee(5, "Swathi"); 
-	String s = "Yash";
+	//String s = "Yash";
 	
 	
 	HashSet <Employee>employees = new HashSet<>();
@@ -28,7 +31,7 @@ public class HelloWorld {
 	employees.add(e2);
 	employees.add(e3);
 	employees.add(e4);
-	employees.add(e5);
+	employees.add(e5);/*
 	for (Employee e : employees)   
 		
 		{System.out.println(e.getEmployeeName());
@@ -38,12 +41,39 @@ public class HelloWorld {
 
     for (Iterator<Employee> it = employees.iterator(); it.hasNext(); )  { 
     	System.out.println(it.next().getEmployeeName());
-          }
+          }*/
+   
+    
+    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
+
+   
+    
+
+    
+    int sum = numbers.parallelStream().reduce(0, (element1,element2)->element1+element2);
+    
+    System.out.println(sum);
+    
+    
+    /*
+    
+    
+    List<String> words = Arrays.asList("Java","SQL","Angular");
+    
+    List<String> result = words.parallelStream().filter(s ->s.startsWith("S")).collect(Collectors.toList());
+    
+    System.out.println(result);
+    
+    
+    
+    
+    List<Integer> square = numbers.parallelStream().map(x->x*x).collect(Collectors.toList());
+    System.out.println(square);
+    
+    */
 	
-	HashMap<Integer, Employee> empMap = new HashMap<>();
-	empMap.put(1, e1);
-	System.out.println(empMap.get(1));
-	
+    
+
 	
 	
 	}
